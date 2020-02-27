1fc8cdafdacac8885c3b31343b2ac344c070d8c2
reiynaldyy <reiynaldy321@gmail.com>
2020-02-27 12:33:11 +0700

    yesss

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+     public function index()
+    {
+        //BUAT QUERY MENGGUNAKAN MODEL PRODUCT, DENGAN MENGURUTKAN DATA BERDASARKAN CREATED_AT
+        //KEMUDIAN LOAD TABLE YANG BERELASI MENGGUNAKAN EAGER LOADING WITH()
+        //ADAPUN CATEGORY ADALAH NAMA FUNGSI YANG NNTINYA AKAN DITAMBAHKAN PADA PRODUCT MODEL
+        $product = Product::with(['category'])->orderBy('created_at', 'DESC');
+
+        //JIKA TERDAPAT PARAMETER PENCARIAN DI URL ATAU Q PADA URL TIDAK SAMA DENGAN KOSONG
+        if (request()->q != '') {
+            //MAKA LAKUKAN FILTERING DATA BERDASARKAN NAME DAN VALUENYA SESUAI DENGAN PENCARIAN YANG DILAKUKAN USER
+            $product = $product->where('name', 'LIKE', '%' . request()->q . '%');
+        }
+        //TERAKHIR LOAD 10 DATA PER HALAMANNYA
+        $product = $product->paginate(10);
+        //LOAD VIEW INDEX.BLADE.PHP YANG BERADA DIDALAM FOLDER PRODUCTS
+        //DAN PASSING VARIABLE $PRODUCT KE VIEW AGAR DAPAT DIGUNAKAN
+        return view('products.index', compact('product'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::group(['prefix' => 'administrator', 'middleware' => 'auth'], function() {
+Route::get('/home', 'HomeController@index')->name('home'); //JADI ROUTING INI SUDAH ADA DARI ARTIKEL SEBELUMNYA TAPI KITA PINDAHKAN KEDALAM GROUPING
+Route::get('/category', 'CategoryController@index')->name('category.index');
+Route::post('/category', 'CategoryController@store')->name('category.store');
+Route::get('/category/{category_id}/edit', 'CategoryController@edit')->name('category.edit');
+Route::put('/category/{category_id}', 'CategoryController@update')->name('category.update');
+Route::delete('/category/{category_id}', 'CategoryController@destroy')->name('category.destroy');
+
+Route::get('/category/{category_id}', 'CategoryController@show')->name('category.show');
+Route::get('/category/create', 'CategoryController@create')->name('category.create');
+
+    //INI ADALAH ROUTE BARU
+    Route::resource('category', 'CategoryController')->except(['create', 'show']);
+    Route::resource('product', 'ProductController');
+});
+
